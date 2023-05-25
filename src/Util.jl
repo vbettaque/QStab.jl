c36470ad48a4a53769f2fc94dfa361902c829448
Valerie Bettaque <v.l.bettaque@gmail.com>
2023-05-25 16:58:13 -0400

    added rref

--- src/Util.jl
+++ src/Util.jl
@@ -8,4 +8,39 @@ end
 
 ⊕(A, B) = directsum(A, B)
 
+function rref(M)
+    lead = 1
+    n_rows, n_columns = size(M)
+    M_rref = copy(M)
+	for r in 1:n_rows
+        if n_columns < lead
+            return M_rref
+		end
+        i = r
+        while iszero(M_rref[i, lead])
+            i += 1
+            if i > n_rows
+                i = r
+                lead += 1
+                if lead > n_columns
+                    return M_rref
+				end
+			end
+		end
+        if i != r
+            temp = copy(M_rref[i, :])
+            M_rref[i, :] = M_rref[r, :]
+            M_rref[r, :] = temp
+		end
+        M_rref[r] /= M_rref[r, lead]
+        for j in 1:n_rows
+            if j != r
+                M_rref[j, :] -= M_rref[j, lead] * M_rref[r, :]
+			end
+		end
+        lead += 1
+	end
+    return M_rref
+end
+
 end
